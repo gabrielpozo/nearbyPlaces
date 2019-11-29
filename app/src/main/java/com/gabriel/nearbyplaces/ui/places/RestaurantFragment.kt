307aff64ed31be09a20ad4535a47325bb9294347
gabriel.pozo.guzman <gabrielpozog@gmail.com>
2019-11-29 12:01:12 +0100

    [implementation] nav_graph added

---
+++ app/src/main/java/com/gabriel/nearbyplaces/ui/places/RestaurantFragment.kt
@@ -0,0 +1,9 @@
+package com.gabriel.nearbyplaces.ui.places
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Gabriel Pozo Guzman on 2019-11-29.
+ */
+class RestaurantFragment: Fragment() {
+}
